ed227a0243b4b1bfab9c641531e7184e234ae976
ryotaro.saji <abcabcxxx01@gmail.com>
2020-02-04 10:21:25 +0000

    2nd task

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_02_185939) do
+ActiveRecord::Schema.define(version: 2020_02_04_095017) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
