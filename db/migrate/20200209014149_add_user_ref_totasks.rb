9016ad83f0433e5099f6129d053486479ac0f05f
ryotaro.saji <abcabcxxx01@gmail.com>
2020-02-09 04:31:26 +0000

    aaa

---
+++ db/migrate/20200209014149_add_user_ref_totasks.rb
@@ -0,0 +1,9 @@
+class AddUserRefTotasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :content,:status
+      t.references :user, foreign_key: true
+
+      t.timestamps
+  end
+end
