1297fff2050887d0ceee1a610726d225b10bbf22
ryotaro.saji <abcabcxxx01@gmail.com>
2020-02-12 13:06:44 +0000

    kore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
-  def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
+ 
 
   def new
     @user = User.new
@@ -16,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to "/"
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
