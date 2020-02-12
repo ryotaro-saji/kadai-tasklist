c47b926a1f6cc469954034e249a5b5b16ec47624
ryotaro.saji <abcabcxxx01@gmail.com>
2020-02-12 12:03:13 +0000

    s

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が登録されませんでした'
-      render 'tasks/index'
+      render :new
     end
   end
 
